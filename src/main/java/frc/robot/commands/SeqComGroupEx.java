0fd8a9798c56d22d1886ed194c75fe5a1ecb4c05
Monykus7 <adityamahesh16@gmail.com>
2022-02-10 22:14:33 -0800

    made add commands easier to understand with extra comments

--- src/main/java/frc/robot/commands/SeqComGroupEx.java
+++ src/main/java/frc/robot/commands/SeqComGroupEx.java
@@ -6,15 +6,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Dumper;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class SeqComGroupEx extends SequentialCommandGroup {
   /** Creates a new AutonOne. */
-  public SeqComGroupEx(DriveTrain dt) {
+  public SeqComGroupEx(DriveTrain dt, Dumper d) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new DriveForwardTimed(dt));
+    addCommands(new DriveForwardTimed(dt), new DumperVomit(d));
+    //Dumper vomit wouldn't work here, since it's binded to a button, but put other auton commands in this format
+    // - addCommands(new *insertcommandhere()*, new *insertcommandhere()*)
+    // For any parameters, add it with the subsystem in the public area bit 
   }
 }
